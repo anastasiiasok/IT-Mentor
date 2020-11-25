966d6c7c788c1a0c67cf35d6581f44449085aa5f
Anastasiia Soktoeva <anastasiiasoktoeva@Anastasiias-Air.fios-router.home>
2020-11-25 14:10:40 +0300

    Education, Person, Projects

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,5 +1,16 @@
+import Education from "../Education/Education";
+import Person from "../Person/Person";
+import Projects from "../Projects/Projects";
+
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <>
+      <div>Profile</div>
+      <Education />
+      <Person />
+      <Projects />
+    </>
+  );
 }
 
 export default Profile;
