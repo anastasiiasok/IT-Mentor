32498db93d0f7f45bf5416cd323daafa1b8ae173
roman <rdubinkin@gmail.com>
2020-11-25 08:18:19 -0500

    setting up to load data

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-require('./src/config/dbConnect.js')
+require('./src/config/dbConnect.js');
 const express = require('express');
 const app = express();
 const cors = require('cors');
---
+++ server/src/config/dbSeed.js
@@ -0,0 +1,8 @@
+require('./src/config/dbConnect.js');
+const Mentor = require('../models/Mentor');
+
+const preloadableStore = {};
+
+const seed = ()=>{
+    const init= 
+}
