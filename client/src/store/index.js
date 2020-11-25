7d0b0498181a54b72ea3f26a6a345c3279d0997b
gayazurna <gayazurna@gmail.com>
2020-11-25 16:15:55 +0300

    preloaded

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,42 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { reducers, initialState } from './reducers';
+import { reducers } from './reducers';
+
+const preloadedState = {
+  name: 'Ivan',
+  surname: 'Petrov',
+  country: 'Russia',
+  city: 'Moscow',
+  email: 'iii@gmail.com',
+  timezone: 'GMT+03',
+  education: [
+    {
+      degree: 'Master',
+      institution: 'MGU',
+    },
+  ],
+  price: 15,
+  lessons: 3,
+  skills: ['JS', 'React'],
+  benefits: ['Help with JS', 'Help with React'],
+  projects: [
+    {
+      title: 'Broccoli Blog',
+      body: 'Online Blog',
+    },
+  ],
+  resume: [
+    {
+      date: {
+        start: '10.03.2018',
+        end: 'now',
+      },
+      company: 'Google',
+      position: 'Front-end developer',
+    },
+  ],
+};
 
 const composeEnhancers = composeWithDevTools();
 
-export const store = createStore(reducers, initialState, composeEnhancers);
+export const store = createStore(reducers, preloadedState, composeEnhancers);
--- client/src/store/reducers.js
+++ client/src/store/reducers.js
@@ -1,1 +1,8 @@
 import { VIEW_PROFILE } from './types';
+
+export const reducers = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
