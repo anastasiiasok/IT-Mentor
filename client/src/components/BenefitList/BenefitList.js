6e92e3534c8462cb1e89ec3fb7fe5371b042bb6b
gayazurna <gayazurna@gmail.com>
2020-11-26 12:32:40 +0300

    Components added

--- client/src/components/BenefitList/BenefitList.js
+++ client/src/components/BenefitList/BenefitList.js
@@ -1,11 +1,14 @@
 import BenefitItem from '../BenefitItem/BenefitItem';
-
+import { useSelector } from 'react-redux';
 function BenefitList() {
+  const benefits = useSelector((state) => state[0].benefits);
   return (
     <div>
       <h3>BenefitList</h3>
       <ul>
-        <BenefitItem />
+        {benefits.map((benefit) => (
+          <BenefitItem benefit={benefit} />
+        ))}
       </ul>
     </div>
   );
