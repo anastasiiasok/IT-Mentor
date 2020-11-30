a7b813702f0c54621ca1273ecb317088cf6a5ca1
gayazurna <gayazurna@gmail.com>
2020-11-30 19:05:31 +0300

    homepage

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
-import SignUp from '../SignUp/SignUp';
+
 function Footer() {
   return (
     <>
       <footer>
         <div className='footerMentors'>
           <p className='footerTitle'>Mentors</p>
-          <p className='footerA'>
-            <a className='#'>Sign In</a>
-          </p>
-          <p className='footerA'>
-            <Link to='/signup'>Become Mentor</Link>
-          </p>
+          <Link to='/signup'>
+            <p className='footerA'>Sign In</p>
+          </Link>
+          <Link to='/signup'>
+            <p className='footerA'>Become Mentor</p>
+          </Link>
         </div>
       </footer>
     </>
