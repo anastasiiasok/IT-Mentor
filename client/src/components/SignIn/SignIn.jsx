426bbc6374f48a6cf397a363bb0b6f0822e62fa4
roman <rdubinkin@gmail.com>
2020-11-30 22:45:20 -0500

    added input fields

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -4,7 +4,14 @@ import styles  from './SignIn.module.css';
 
 const SignIn = ()=>{
   return(
-    <div className={styles.container}></div>
+    <div className={styles.container}>
+      <input className={styles.field} placeholder="first name" type="text"/>
+      <input className={styles.field} placeholder="last name" type="text"/>
+      <input className={styles.field} placeholder="email" type="text"/>
+      <div style={{height: "100px"}}></div>
+      <input className={styles.field} placeholder="password" type="text"/>
+      <input className={styles.field} placeholder="confirm password" type="text"/>
+    </div>
   )
 }
 
