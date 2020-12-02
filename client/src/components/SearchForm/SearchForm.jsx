33018eb55be28b3cc34cc23a293a1c39f36c1124
Anastasiia Soktoeva <anastasiiasoktoeva@Anastasiias-Air.fios-router.home>
2020-12-02 17:13:07 +0300

    summary, education (degree & institution)

--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -1,18 +1,16 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import Select from "react-select";
+import { SCREEN_SIZE } from "../../store/types";
 
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import Select from 'react-select';
-import { SCREEN_SIZE } from '../../store/types';
+import styles from "./SearchForm.module.css";
 
-
-import styles from './SearchForm.module.css';
-
-import { searchMentors, setScreen } from '../../store/actions';
+import { searchMentors, setScreen } from "../../store/actions";
 
 const customStyles = {
   multiValue: (provided, state) => ({
     ...provided,
-    borderBottom: '1px solid green',
+    borderBottom: "1px solid green",
     color: state.selectProps.menuColor,
   }),
 };
@@ -26,17 +24,15 @@ function SearchForm() {
 
   const dispatch = useDispatch();
   const tags = [
-
-    { value: 'express', label: 'Express', isFixed: true, color: 'pink' },
-    { value: 'postman', label: 'Postman' },
-    { value: 'nodejs', label: 'NodeJS' },
-    { value: 'restapi', label: 'REST API' },
-    { value: 'saga', label: 'Saga' },
-    { value: 'react', label: 'React' },
-    { value: 'mongodb', label: 'MongoDB' },
-    { value: 'sass', label: 'Sass' },
-    { value: 'ubuntu', label: 'Ubuntu' },
-
+    { value: "express", label: "Express", isFixed: true, color: "pink" },
+    { value: "postman", label: "Postman" },
+    { value: "nodejs", label: "NodeJS" },
+    { value: "restapi", label: "REST API" },
+    { value: "saga", label: "Saga" },
+    { value: "react", label: "React" },
+    { value: "mongodb", label: "MongoDB" },
+    { value: "sass", label: "Sass" },
+    { value: "ubuntu", label: "Ubuntu" },
   ];
   const tagOptions = tags.map((tag) => ({ ...tag, value: tag.label }));
 
@@ -50,10 +46,9 @@ function SearchForm() {
 
   const handleClick = async () => {
     const queryArr = [];
-    const key = '1288100a174d447583eb11e7a4ab6d2c';
+    const key = "1288100a174d447583eb11e7a4ab6d2c";
 
-    if (filters.length) queryArr.push('skills=' + filters.join(','));
-
+    if (filters.length) queryArr.push("skills=" + filters.join(","));
 
     if (checkedItems.timezone) {
       const res = await fetch(
@@ -64,19 +59,21 @@ function SearchForm() {
       queryArr.push(`timezone=${timezoneData.time_zone.offset}`);
     }
 
-
     if (checkedItems.price)
       queryArr.push(`price=${checkedItems.down ? -1 : 1}`);
-    const query = queryArr.join('&');
+    const query = queryArr.join("&");
 
     console.log(query);
     const repsonse = await fetch(`http://localhost:3100/mentor?${query}`);
     const mentors = await repsonse.json(); // { [{}]} object with array of objects
     dispatch(setScreen(SCREEN_SIZE));
-    dispatch(searchMentors({mentors: mentors.mentors.map((mentor)=>({...mentor, liked: false}))}));
+    dispatch(
+      searchMentors({
+        mentors: mentors.mentors.map((mentor) => ({ ...mentor, liked: false })),
+      })
+    );
   };
   return (
-
     <div className={styles.searchMain}>
       <div className={styles.searchDiv}>
         <div className={styles.search}>
@@ -85,10 +82,10 @@ function SearchForm() {
             value={tagOptions.filter((obj) => filters.includes(obj.value))}
             // defaultValue={[tagOptions[2], tagOptions[3]]}
             isMulti
-            name='skills'
+            name="skills"
             options={tagOptions}
-            className='basic-multi-select'
-            classNamePrefix='select'
+            className="basic-multi-select"
+            classNamePrefix="select"
             className={styles.selectItem}
             isClearable
           />
@@ -96,26 +93,24 @@ function SearchForm() {
         <div className={styles.searchButton}>
           <button onClick={handleClick}>Search</button>
         </div>
-
       </div>
       <div className={styles.checkboxMain}>
         <label className={styles.label}>
           <input
             className={styles.checkInput}
-            name='price'
-            type='checkbox'
+            name="price"
+            type="checkbox"
             onChange={handleChangeCheckbox}
           />
           Price
         </label>
 
-
         <label className={styles.label}>
           <input
             className={styles.checkInput}
             disabled={!checkedItems.price}
-            name='down'
-            type='checkbox'
+            name="down"
+            type="checkbox"
             onChange={handleChangeCheckbox}
           />
           Down
@@ -123,14 +118,13 @@ function SearchForm() {
         <label className={styles.label}>
           <input
             className={styles.checkInput}
-            name='timezone'
-            type='checkbox'
+            name="timezone"
+            type="checkbox"
             onChange={handleChangeCheckbox}
           />
           Timezone
         </label>
       </div>
-
     </div>
   );
 }
