cbc3b7bbb6228931257d9b224e73f82da6462060
gayazurna <gayazurna@gmail.com>
2020-12-04 00:15:03 +0300

    rebuild

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -1,78 +1,130 @@
 import React from 'react';
-import styles from './SignIn.module.css'
-import {useSelector} from 'react-redux';
-import {useHistory} from 'react-router-dom';
+import styles from './SignIn.module.css';
+import { useSelector } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
 
-const SignIn = ({visible, setVisibility})=>{
+const SignIn = () => {
   const history = useHistory();
-  const init = {firstName: '', lastName: '', contacts: '', password: '', confirm: ''};
+  const init = {
+    firstName: '',
+    lastName: '',
+    contacts: '',
+    password: '',
+    confirm: '',
+  };
   const [form, updateForm] = React.useState(init);
-  const mentors = useSelector((store)=>store.likedMentors);
-
-  const handleSubmit = async ()=>{
+  const mentors = useSelector((store) => store.likedMentors);
+  const handleClick = () => {
+    history.goBack();
+  };
+  const handleSubmit = async () => {
     if (form.password !== '') {
+      // ACHTUNG !!! COMMENT NEXT LINE BEFORE BUILD!!
+      const res = await fetch('http://localhost:3100/user/auth/local', {
+        // <<<<<<<<<<<<<<<<<< DON'T TOUCH THIS >>>>>>>>>
+        // ACHTUNG !!! UNCOMMENT NEXT LINE BEFORE BUILD!!!!
+        // const res = await fetch('https://servertestmentor.herokuapp.com/user/auth/local', {
 
-    // ACHTUNG !!! COMMENT NEXT LINE BEFORE BUILD!!
-    const res = await fetch('http://localhost:3100/user/auth/local', {
-  // <<<<<<<<<<<<<<<<<< DON'T TOUCH THIS >>>>>>>>>
-    // ACHTUNG !!! UNCOMMENT NEXT LINE BEFORE BUILD!!!!
-    // const res = await fetch('https://servertestmentor.herokuapp.com/user/auth/local', {
+        // <<<<<<<<<<<<<<<<<< DON'T TOUCH THIS >>>>>>>>>
+        method: 'POST',
+        // DO NOT USE MODE NO-CORS !!!
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...form, mentors }),
+      });
+      const data = await res.json();
+      console.log(data);
+      updateForm(init);
 
-    // <<<<<<<<<<<<<<<<<< DON'T TOUCH THIS >>>>>>>>>
-      method: "POST",
-      // DO NOT USE MODE NO-CORS !!!
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({...form, mentors}),
-    });
-    const data = await res.json();
-    console.log(data);
-    updateForm(init);
-    setVisibility(false);
-    history.push("/account");
-  } else {
-    updateForm(init);
-    setVisibility(false);
-  }
+      history.push('/account');
+    } else {
+      updateForm(init);
+    }
   };
 
   const handleChange = (e) => {
-    updateForm({...form, [e.target.name]: e.target.value});
+    updateForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleGoogle = async ()=>{
-
+  const handleGoogle = async () => {
     // ACHTUNG !!! COMMENT NEXT LINE BEFORE BUILD!!
     const res = await fetch('http://localhost:3100/user/auth/google', {
- // <<<<<<<<<<<<<<<<<< DON'T TOUCH THIS >>>>>>>>>
+      // <<<<<<<<<<<<<<<<<< DON'T TOUCH THIS >>>>>>>>>
 
-     // ACHTUNG !!! UNCOMMENT NEXT LINE BEFORE BUILD!!!!
-    //  const res = await fetch('https://servertestmentor.herokuapp.com/user/auth/google', {
+      // ACHTUNG !!! UNCOMMENT NEXT LINE BEFORE BUILD!!!!
+      //  const res = await fetch('https://servertestmentor.herokuapp.com/user/auth/google', {
 
-     // <<<<<<<<<<<<<<<<<< DON'T TOUCH THIS >>>>>>>>>
-     
-      method: "GET",
+      // <<<<<<<<<<<<<<<<<< DON'T TOUCH THIS >>>>>>>>>
+
+      method: 'GET',
       mode: 'no-cors',
     });
     const data = await res.json();
     console.log(data);
   };
 
-  return(
-    <div className={styles.container} style={{visibility: visible ? "visible" : "hidden"}}>
-      <input onChange={handleChange} value={form.firstName} name="firstName" placeholder="first name" type="text"/>
-      <input  onChange={handleChange} value={form.lastName} name="lastName" placeholder="last name" type="text"/>
-      <input  onChange={handleChange} value={form.contacts} name="contacts" placeholder="email" type="text"/>
-      <div style={{height: "100px"}}></div>
-      <input   onChange={handleChange} value={form.password} name="password" placeholder="password" type="text"/>
-      <input  onChange={handleChange} value={form.confirm} name="confirm" placeholder="confirm password" type="text"/>
+  const left = useLocation().state.left;
+  const bottom = useLocation().state.bottom;
+  return (
+    <div className={styles.container} style={{ left: left, bottom: bottom }}>
+      <div
+        style={{
+          width: '95%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+        }}
+      >
+        <i onClick={handleClick} class='fas fa-times'></i>
+      </div>
+      <input
+        onChange={handleChange}
+        value={form.firstName}
+        name='firstName'
+        placeholder='first name'
+        type='text'
+      />
+      <input
+        onChange={handleChange}
+        value={form.lastName}
+        name='lastName'
+        placeholder='last name'
+        type='text'
+      />
+      <input
+        onChange={handleChange}
+        value={form.contacts}
+        name='contacts'
+        placeholder='email'
+        type='text'
+      />
+      <div style={{ height: '100px' }}></div>
+      <input
+        onChange={handleChange}
+        value={form.password}
+        name='password'
+        placeholder='password'
+        type='text'
+      />
+      <input
+        onChange={handleChange}
+        value={form.confirm}
+        name='confirm'
+        placeholder='confirm password'
+        type='text'
+      />
       <div className={styles.button_container}>
-        <button  onClick={handleSubmit}>Submit</button>
-        <button  style={{backgroundColor: "rgba(243, 192, 73, 1)"}} onClick={handleGoogle}>Sign In with Google</button>
+        <button onClick={handleSubmit}>Submit</button>
+        <button
+          style={{ backgroundColor: 'rgba(243, 192, 73, 1)' }}
+          onClick={handleGoogle}
+        >
+          Sign In with Google
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignIn;
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,52 +1,49 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { reducers } from "./reducers";
-import { SCREEN_SIZE } from "./types";
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { reducers } from './reducers';
+import { SCREEN_SIZE } from './types';
 
-const preloadedState = { mentors: [
-  {
-
-    name: 'Ivan',
-    surname: 'Petrov',
-    country: 'Russia',
-    city: 'Moscow',
-    email: 'iii@gmail.com',
-    timezone: 'GMT+03',
-    education: [
-      {
-        degree: 'Master',
-        institution: 'MGU',
-
-      },
-    ],
-    price: 15,
-    lessons: 3,
-    skills: ['JS', 'React'],
-
-    benefits: ['Help with JS', 'Help with React'],
-    projects: [
-      {
-        title: 'Broccoli Blog',
-        body: 'Online Blog',
-
-      },
-    ],
-    resume: [
-      {
-        date: {
-
-          start: '2018-10-03',
-          end: 'now',
+const preloadedState = {
+  mentors: [
+    {
+      name: 'Ivan',
+      surname: 'Petrov',
+      country: 'Russia',
+      city: 'Moscow',
+      email: 'iii@gmail.com',
+      timezone: 'GMT+03',
+      education: [
+        {
+          degree: 'Master',
+          institution: 'MGU',
         },
-        company: 'Google',
-        position: 'Front-end developer',
+      ],
+      price: 15,
+      lessons: 3,
+      skills: ['JS', 'React'],
 
-      },
-    ],
-  },
-],
-screen: SCREEN_SIZE,
-likedMentors: [],
+      benefits: ['Help with JS', 'Help with React'],
+      projects: [
+        {
+          title: 'Broccoli Blog',
+          body: 'Online Blog',
+        },
+      ],
+      resume: [
+        {
+          date: {
+            start: '2018-10-03',
+            end: 'now',
+          },
+          company: 'Google',
+          position: 'Front-end developer',
+        },
+      ],
+    },
+  ],
+  screen: SCREEN_SIZE,
+  likedMentors: [],
+  activeMentor: {},
 };
 
 const composeEnhancers = composeWithDevTools();
